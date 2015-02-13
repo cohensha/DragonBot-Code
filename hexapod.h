17159667ea5af8533da0732191f91d57be61104a
Cindy Tran <trancind@usc.edu>
2015-02-13 11:24:53 -0800

    took out the self

--- hexapod.h
+++ hexapod.h
@@ -1,5 +1,25 @@
 #ifndef HEXAPOD_H
 #define HEXAPOD_H
+#include "vector3.h"
+
+//measurements based on computer model of robot
+double BICEP_LENGTH = 5.000;
+double FOREARM_LENGTH =16.750;
+double SWITCH_ANGLE = 19.690;
+//top center to wrist hole: X7.635 Y+/-0.553 Z0.87
+double T2W_X = 7.635;
+double T2W_Y = 0.553;
+double T2W_Z = -0.870;
+//base center to shoulder hole: X8.093 Y+/-2.15 Z7.831
+double B2S_X = 8.093;
+double B2S_Y = 2.150;
+double B2S_Z = 6.618;
+
+//some experimentally determined limits on in/out distance (distance along motor shaft direction)
+double MAX_IN = 4.46;
+double MAX_OUT = 4.25;
+double POS_STEP = 0.1;
+double RPY_STEP = 1;
 
 class Hexapod{
 
@@ -28,10 +48,6 @@ public:
 
 	Hexapod * get_pos();
 
-	
-        
-
-
 private:
 
 	//variables
@@ -39,18 +55,18 @@ private:
 	double shoulders[6];
 	double elbows[6];
 	double shoulder_to_elbow [6];
-    double self.ee_pos;
-    double self.ee_rpy;
-    double self.ee_up;
-    double self.ee_fw;
-    double self.ee_left;
-    double self.angles [6];
-    double self.rel_z;
+    double ee_pos;
+    double ee_rpy;
+    double ee_up;
+    double ee_fw;
+    double ee_left;
+    double angles [6];
+    double rel_z;
 
     double el;
     double wr;
     double aa;
     double cc;
     double bb;
-    double self.rel_z
+    double rel_z
 }
