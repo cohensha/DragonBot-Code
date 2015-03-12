0622cd98e056390ec7f04901071752d9ff829687
Sharon Cohen <cohensha@usc.edu>
2015-03-12 14:14:26 -0700

    added hexapod tests

--- hexapodtest.cpp
+++ hexapodtest.cpp
@@ -7,11 +7,18 @@ int main(){
 	
 	Hexapod * a = new Hexapod;
 
-	a->update_end_effector(1,1,1,2,2,2);
-
 	Vector3 position = a->get_pos();
+	cout << "initial length: " << position.length() << endl;
+	cout << " " << endl;
 
-	cout << "length: " << position.length() << endl;
+	a->update_end_effector(3,1,4,2,2,2);
+	Vector3 position1 = a->get_pos();
+	cout << "length: " << position1.length() << endl;
+	cout << " " << endl;
+
+	a->update_ik(1,1,1,2,2,2);
+	Vector3 rpy = a->get_rpy();
+	cout << "x value of rpy: " << rpy.getitem(0.0) << endl;
 
 	return 0;
 }
