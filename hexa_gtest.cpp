ec8d0fa64c9e83b94cfeca706ea2b082a5277db5
Cindy Tran <trancind@usc.edu>
2015-04-17 11:19:23 -0700

    more tests

--- hexa_gtest.cpp
+++ hexa_gtest.cpp
@@ -24,7 +24,7 @@ TEST_F(HexaTest, Less_Arg_Check_IK) {
 	EXPECT_EQ(h.check_ik('x',4,'y',4,'z',4,'u',4), false);
 }
 
-TEST_F(HexaTest, Nominal_Best_IK) {
+TEST_F(HexaTest, Off_Nominal_Best_IK) {
 	Hexapod h=Hexapod();
 	double x[6];
 	h.best_effort_ik(x,'x',-4,'y',-4,'z',-4,'u',-4,'v',-4,'w',-4);
@@ -34,7 +34,37 @@ TEST_F(HexaTest, Nominal_Best_IK) {
 	EXPECT_EQ(x[3],-0.5358983848622456);
 	EXPECT_EQ(x[4],-0.5358983848622456);
 	EXPECT_EQ(x[5],-0.5358983848622456);
+
+	h.best_effort_ik(x,'x',10,'y',10,'z',10,'u',10,'v',10,'w',10);
+	EXPECT_EQ(x[0],2.898591688967633);
+	EXPECT_EQ(x[1],2.898591688967633);
+	EXPECT_EQ(x[2],2.898591688967633);
+	EXPECT_EQ(x[3],0.1850454237763578);
+	EXPECT_EQ(x[4],0.1850454237763578);
+	EXPECT_EQ(x[5],0.1850454237763578);
 }
+
+TEST_F(HexaTest, Nominal_Best_IK) {
+	Hexapod h=Hexapod();
+	double x[6];
+	h.best_effort_ik(x,'x',0,'y',0,'z',0,'u',0,'v',0,'w',0);
+	EXPECT_EQ(x[0],0);
+	EXPECT_EQ(x[1],0);
+	EXPECT_EQ(x[2],0);
+	EXPECT_EQ(x[3],0);
+	EXPECT_EQ(x[4],0);
+	EXPECT_EQ(x[5],0);
+
+	h.best_effort_ik(x,'x',2,'y',2,'z',2,'u',2,'v',2,'w',2);
+	EXPECT_EQ(x[0],2);
+	EXPECT_EQ(x[1],2);
+	EXPECT_EQ(x[2],2);
+	EXPECT_EQ(x[3],2);
+	EXPECT_EQ(x[4],2);
+	EXPECT_EQ(x[5],2);
+}
+
+//TODO: check angles!
 
 
 
